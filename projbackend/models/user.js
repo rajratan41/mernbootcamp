e0724b3062ce1b870daedcf7b3246fd0eef58bf6
Raj Ratan <raj808341@gmail.com>
2023-08-23 17:47:41 +0530

    added timestamps

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -2,42 +2,45 @@ const mongoose = require("mongoose");
 const crypto = require("node:crypto");
 const { v4: uuidv4 } = require("uuid");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    maxLength: 32,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxLength: 32,
+      trim: true,
+    },
+    lastname: {
+      type: String,
+      maxLength: 32,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    userInfo: {
+      type: String,
+      trim: true,
+    },
+    encry_password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    role: {
+      type: Number,
+      default: 0,
+    },
+    purchases: {
+      type: Array,
+      default: [],
+    },
   },
-  lastname: {
-    type: String,
-    maxLength: 32,
-    trim: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    unique: true,
-    required: true,
-  },
-  userInfo: {
-    type: String,
-    trim: true,
-  },
-  encry_password: {
-    type: String,
-    required: true,
-  },
-  salt: String,
-  role: {
-    type: Number,
-    default: 0,
-  },
-  purchases: {
-    type: Array,
-    default: [],
-  },
-});
+  { timestamps: true }
+);
 
 userSchema
   .virtual("password")
