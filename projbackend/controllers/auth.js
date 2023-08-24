d323f2c119bdd34371de7b3fe03c88795dbb4885
Raj Ratan <raj808341@gmail.com>
2023-08-24 18:31:34 +0530

    saving user to DB

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,3 +1,23 @@
+const User = require("../models/user");
+
+exports.signup = (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then((savedUser) => {
+      res.json({
+        name: user.name,
+        email: user.email,
+        id: user._id,
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({
+        error: err.message,
+      });
+    });
+};
+
 exports.signout = (req, res) => {
   res.json({
     message: "User Signout",
