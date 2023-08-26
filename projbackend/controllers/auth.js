a621d106c0be43e5dd186d7a99089d61ff165e4c
Raj Ratan <raj808341@gmail.com>
2023-08-26 22:44:54 +0530

    how to write custom middleware

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -81,3 +81,23 @@ exports.isSignedIn = expressjwt({
 });
 
 // custom middlewares
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+  if (!checker) {
+    return res.status(403).json({
+      error: "ACCESS DENIED",
+    });
+  }
+
+  next();
+};
+
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error: "You are not ADMIN, ACCESS DENIED",
+    });
+  }
+
+  next();
+};
