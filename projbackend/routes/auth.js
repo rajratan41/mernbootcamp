40a053295795b23c39db64b1566d6b00f200ed0b
Raj Ratan <raj808341@gmail.com>
2023-08-26 22:44:07 +0530

    res.json to see what is in req.auth

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -23,8 +23,9 @@ router.post(
 );
 
 router.get("/signout", signout);
+
 router.get("/testroute", isSignedIn, (req, res) => {
-  res.send("A protected route");
+  res.json(req.auth);
 });
 
 module.exports = router;
