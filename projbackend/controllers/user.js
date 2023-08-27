9025306e976b34ba3c763e1e3d6eafa705d13dcb
Raj Ratan <raj808341@gmail.com>
2023-08-27 20:44:37 +0530

    Fixing a major bug

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -18,5 +18,7 @@ exports.getUserById = (req, res, next, id) => {
 
 exports.getUser = (req, res) => {
   // TODO: get back here for password
+  req.profile.salt = undefined;
+  req.profile.encry_password = undefined;
   return res.json(req.profile);
 };
