94d48d8d7113c5d887266138655cd4a676f8afc8
Raj Ratan <raj808341@gmail.com>
2023-08-27 19:52:57 +0530

    Get ID from Params

---
+++ projbackend/controllers/user.js
@@ -0,0 +1,22 @@
+const User = require("../models/user");
+
+// Get id of User from Params
+exports.getUserById = (req, res, next, id) => {
+  User.findById(id)
+    .then((user) => {
+      req.profile = user;
+      next();
+    })
+    .catch((err) => {
+      if (err) {
+        return res.status(400).json({
+          error: "No User was Found in DB",
+        });
+      }
+    });
+};
+
+exports.getUser = (req, res) => {
+  // TODO: get back here for password
+  return res.json(req.profile);
+};
