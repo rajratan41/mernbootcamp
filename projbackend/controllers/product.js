20733e17bbb422fe9acc3d08a9493cd5c3342fb7
Raj Ratan <raj808341@gmail.com>
2023-09-02 20:51:30 +0530

    saving photo in mongo

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -1,4 +1,7 @@
 const Product = require("../models/product");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
 
 exports.getProductById = async (req, res, next, id) => {
   try {
@@ -8,6 +11,44 @@ exports.getProductById = async (req, res, next, id) => {
   } catch (error) {
     res.status(400).json({
       message: "Product Not Found",
+      error: error.message,
+    });
+  }
+};
+
+exports.createProduct = async (req, res) => {
+  try {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+
+    form.parse(req, (err, fields, file) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Problem with image",
+        });
+      }
+
+      //   TODO: restrictions on fields
+      let product = new Product(fields);
+
+      //   handle file here
+      if (file.photo) {
+        if (file.photo.size > 3000000) {
+          return res.status(400).json({
+            error: "File size is too big!",
+          });
+        }
+        product.photo.data = fs.readFileSync(file.photo.path);
+        product.photo.contentType = file.photo.type;
+      }
+
+      //   save to the db
+      product.save();
+      res.json(product);
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Saving tshirt in DB failed",
       error: error.message,
     });
   }
